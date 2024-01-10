17683328cde97b0eae2a8f95263747d3c2bb864b
RubenErhardt <rubengamebattles@outlook.com>
2024-01-10 23:23:06 +0100

    oki

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,27 @@
 document.getElementById('openMenu').addEventListener('click', function() {
   const menuOverlay = document.getElementById('menuOverlay');
   const menuContent = document.querySelector('.menu-content');
+  const images = document.querySelectorAll('.groepsfoto img');
 
   menuOverlay.classList.add('show');
   menuContent.classList.add('show');
+
+  // Hide images
+  images.forEach(img => {
+    img.style.display = 'none';
+  });
 });
 
 document.getElementById('closeMenu').addEventListener('click', function() {
   const menuOverlay = document.getElementById('menuOverlay');
   const menuContent = document.querySelector('.menu-content');
+  const images = document.querySelectorAll('.groepsfoto img');
 
   menuOverlay.classList.remove('show');
   menuContent.classList.remove('show');
+
+  // Show images
+  images.forEach(img => {
+    img.style.display = 'block';
+  });
 });
-
-
