407c4080dda4580e921b210e7158881abf9e7646
RubenErhardt <rubengamebattles@outlook.com>
2024-01-10 12:11:18 +0100

    vet cool wauw

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+document.getElementById('openMenu').addEventListener('click', function() {
+    document.getElementById('menuOverlay').style.display = 'block';
+  });
+  
+  document.getElementById('closeMenu').addEventListener('click', function() {
+    document.getElementById('menuOverlay').style.display = 'none';
+  });
