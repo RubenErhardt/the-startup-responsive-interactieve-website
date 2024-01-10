70bcae7a9aa021d1f2d68a54f5a7b2c4acc7d89f
RubenErhardt <rubengamebattles@outlook.com>
2024-01-10 21:24:42 +0100

    menutje effect met betere java

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,16 @@
 document.getElementById('openMenu').addEventListener('click', function() {
-    document.getElementById('menuOverlay').style.display = 'block';
-  });
-  
-  document.getElementById('closeMenu').addEventListener('click', function() {
-    document.getElementById('menuOverlay').style.display = 'none';
-  });
+  const menuOverlay = document.getElementById('menuOverlay');
+  const menuContent = document.querySelector('.menu-content');
+
+  menuOverlay.classList.add('show');
+  menuContent.classList.add('show');
+});
+
+document.getElementById('closeMenu').addEventListener('click', function() {
+  const menuOverlay = document.getElementById('menuOverlay');
+  const menuContent = document.querySelector('.menu-content');
+
+  menuOverlay.classList.remove('show');
+  menuContent.classList.remove('show');
+});
+
